e169a719c49034102c24f4c615eda8288fce0dc7
atomicabdul4life <abdullateefabdulkareem60@gmail.com>
2022-11-13 12:07:05 +0000

    airbnb v2 updated

--- models/state.py
+++ models/state.py
@@ -1,62 +1,40 @@
-def bubble(list):
-    leng = len(list)
-    for j in range(leng):
-        for i in range(leng - 1):
-            if list[i] > list[i+1]:
-                list[i], list[i+1] = list[i+1], list[i]
-    return list
+#!/usr/bin/python3
+"""
+    Implementation of the State class
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
+import models
 
-def merge(list):
-    leng = len(list)
-    if leng > 1:
-        mid = leng//2
-        list1 = list[:mid]
-        list2 = list[mid:]
-        
-        merge(list1)
-        merge(list2)
-        i=j=k=0
-        while (i < len(list1) and j < len(list2)):
-            if(list1[i] < list2[j]):
-                list[k] = list1[i]
-                i+=1
-            else:
-                list[k] = list2[j]
-                j+=1
-            k+=1
-        while(i<len(list1)):
-            list[k] = list1[i]
-            i+=1
-            k+=1
-        while(j<len(list2)):
-            list[k] = list2[j]
-            j+=1
-            k+=1
 
-def insertion(list):
-    leng = len(list)
-    for i in range(1, leng):
-        j = i
-        while(list[j-1] > list[j] and j>0):
-            list[j-1], list[j] = list[j], list[j-1]
-            j-=1
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
-def selection(list):
-    leng = len(list)
-    for i in range(leng - 1):
-        j = i+1
-        while(j < leng):
-            if list[i] > list[j]:
-                list[i], list[j] = list[j], list[i]
-            j+=1
-    
-            
 
-list = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
-print(bubble(list))
-print(list)
-#insertion(list)
-#merge(list)
-#selection(list)
-#quicksort(list)
-print(list)
+class State(BaseModel, Base):
+    '''
+        Implementation for the State.
+    '''
+    __tablename__ = 'states'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+        name = ""
+
+    if storage_type != 'db':
+        @property
+        def cities(self):
+            """
+            get list of City instances with state_id
+            equals to the current State.id
+            """
+            list_cities = []
+            all_cities = models.storage.all(City)
+            for key, city_obj in all_cities.items():
+                if city_obj.state_id == self.id:
+                    list_cities.append(city_obj)
+            return list_cities
